7afdeb56d515664ab89d562b8b4a3a5c124ca4b3
Nathan Pickard <nathan.pickard87@gmail.com>
2026-03-08 16:03:22 -0700

    Astro scaffolding

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,18 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [
+        mdx(),
+        sitemap(),
+        icon({
+            include: {
+                local: ['src/icons'],
+            },
+        }),
+    ],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nathan Pickard';
+export const SITE_DESCRIPTION = 'Nathan Pickard - Welcome to my website!';
