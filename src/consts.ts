fd917b2f681d833f137a4fb241798ac675e4a588
Nathan Pickard <nathan.pickard87@gmail.com>
2026-04-08 16:56:20 -0700

    Resume page and json scaffolding, Bluesky feed on homepage

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Nathan Pickard';
-export const SITE_DESCRIPTION = 'Nathan Pickard - Welcome to my website!';
+export const SITE_DESCRIPTION = 'Nathan Pickard - Welcome to my personal site!';
